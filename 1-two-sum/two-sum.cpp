d31ee64afadec9982cee600c1ca8eaee2b05f2c2
Razan-J-Mohammad <razanmohmj20@gmail.com>
2024-08-12 12:34:49 +0300

    Time: 109 ms (15.59%) | Memory: 12.9 MB (64.37%) - LeetSync

---
+++ 1-two-sum/two-sum.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+       vector<int>index;
+       for(int i=0;i<nums.size();i++){
+        for(int j=1+i;j<nums.size();j++){
+           if(nums[i]+nums[j]==target){
+             index.push_back(i);
+            index.push_back(j);
+            return index;
+           } 
+        }
+       }
+       return  index;
+
+    }
+};
