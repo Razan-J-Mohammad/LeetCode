90abcbe0196090d67f8414328227421a11eebd62
Razan-J-Mohammad <razanmohmj20@gmail.com>
2024-08-17 17:21:22 +0300

    Time: 173 ms (30.87%) | Memory: 102 MB (39.91%) - LeetSync

---
+++ 380-insert-delete-getrandom-o1/insert-delete-getrandom-o1.cpp
@@ -0,0 +1,40 @@
+
+
+class RandomizedSet {
+private:
+    std::vector<int> nums; 
+    std::unordered_map<int, int> valToIndex; 
+
+public:
+    RandomizedSet() {}
+
+    bool insert(int val) {
+        if (valToIndex.find(val) != valToIndex.end())
+            return false;
+
+        nums.push_back(val);
+        valToIndex[val] = nums.size() - 1;
+        return true;
+    }
+
+    bool remove(int val) {
+        if (valToIndex.find(val) == valToIndex.end())
+            return false;
+
+        int index = valToIndex[val];
+        int lastElement = nums.back();
+
+        nums[index] = lastElement;
+        valToIndex[lastElement] = index;
+
+        nums.pop_back();
+        valToIndex.erase(val);
+
+        return true;
+    }
+
+    int getRandom() {
+        int randomIndex = rand() % nums.size();
+        return nums[randomIndex];
+    }
+};
