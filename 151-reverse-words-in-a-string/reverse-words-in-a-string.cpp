a0b2e511aa3c0c241d68c96cbffb73783c1a2079
Razan-J-Mohammad <razanmohmj20@gmail.com>
2024-09-02 17:11:11 +0300

    Time: 7 ms (35.05%) | Memory: 9.8 MB (60.24%) - LeetSync

---
+++ 151-reverse-words-in-a-string/reverse-words-in-a-string.cpp
@@ -0,0 +1,29 @@
+#include <stack>
+#include <string>
+#include <sstream>
+#include <algorithm>
+
+class Solution {
+public:
+    std::string reverseWords(std::string s) {
+        std::stack<std::string> words;
+        std::string word;
+        std::stringstream ss(s);
+        
+        while (ss >> word) {
+            words.push(word);
+        }
+
+        std::string result;
+        
+        while (!words.empty()) {
+            result += words.top();
+            words.pop();
+            if (!words.empty()) {
+                result += " ";
+            }
+        }
+        
+        return result;
+    }
+};
